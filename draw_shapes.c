2a0742157380b8425cb19224be1e34f2cf64136f
cdsegapeli <cdsegapeli@miners.utep.edu>
2021-02-14 17:46:59 -0700

    modified Makefile to aslo create .o files for 5x7-fonts.c and 8x12-fonts.c; modified draw_chars.c to print 5x7 and 8x12 characters; modified uimain.c to print 11x16, 5x7, and 8x12 characters in succession as well as the ability to print an arrow; modified draw_shapes.c with the print_arrow function.

--- draw_shapes.c
+++ draw_shapes.c
@@ -4,7 +4,6 @@
 /* Prints a size x size square whose left col is at startCol */
 void print_square(int leftCol, int size)
 {
-  int i, j;
   int endCol = leftCol + size;
   for (int row = 0; row < size; row++){
     int col;
@@ -24,5 +23,13 @@ void print_triangle(int leftCol, int size)
     for (       ; col <= maxCol; col++) putchar('*');
     putchar('\n');
   }
+}
+
+// Prints an arrow of specified height
+void print_arrow(int leftCol, int size)
+{
+  print_triangle(leftCol, size);
+  print_square(leftCol+(size-2), size-2);
+  putchar('\n');
 }
 
